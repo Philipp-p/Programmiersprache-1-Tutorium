b5b5c347e37a6ff6cccac390ac68b7dff8ed7bf3
Philipp-p <Philipp-p@users.noreply.github.com>
2018-11-05 16:43:03 +0100

    added examples for function calls

---
+++ function_calls.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+
+int sum_return_value(int a, int b) {
+    return a + b;
+}
+
+void sum_reference(int& a, int b) {
+    a = a + b;
+    b = b + 10;
+}
+
+void print_values(int a, int b) {
+    std::cout << "a: " << a << " b: " << b << '\n';
+}
+int main() {
+    int a = 3;
+    int b = 5;
+    int result = sum_return_value(a, b);
+    print_values(a,b);
+    std::cout << "result: " << result << '\n';
+    sum_reference(a, b);
+    print_values(a, b);
+}
+
+
